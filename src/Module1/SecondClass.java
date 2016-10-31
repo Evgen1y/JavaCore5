b8faf0cb810fdd165ed2b29f4ef6d2883f30ccdb
Evgen1y <bulovackiy@outlook.com>
2016-10-31 10:10:58 +0200

    Add Module1 Home Work1

--- src/Module1/SecondClass.java
+++ src/Module1/SecondClass.java
@@ -1,8 +1,5 @@
 package Module1;
 
-/**
- * Created by bulov on 31.10.2016.
- */
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Мой первый класс на Java");
