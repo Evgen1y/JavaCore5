fcb1663b2bae8b24573a8e441fb79b113583fd1a
Evgen1y <bulovackiy@outlook.com>
2016-10-31 09:59:06 +0200

    test

---
+++ src/Module1/Fifth.java
@@ -0,0 +1,10 @@
+package Module1;
+
+/**
+ * Created by bulov on 31.10.2016.
+ */
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
